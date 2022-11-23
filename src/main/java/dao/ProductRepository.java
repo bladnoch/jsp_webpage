fdfe4045d970c584d6f00798e0fb6453af7a2b76
DK <dongwook5478@gmail.com>
2022-11-23 10:16:33 +0900

    change gram image

--- src/main/java/dao/ProductRepository.java
+++ src/main/java/dao/ProductRepository.java
@@ -44,7 +44,7 @@ public class ProductRepository {
 		nbook.setManufacturer("Lg");
 		nbook.setUnitsInStock(1000);
 		nbook.setCondition("Refurbished");
-		nbook.setFilename("gram.png");
+		nbook.setFilename("gram.jpeg");
 		
 		Product tablet=new Product("P1236", "Galaxy Tab S", 900000);
 		tablet.setDescription("212.8*125.6*6.6mm, Super AMOLED display, OctaCore processor");
